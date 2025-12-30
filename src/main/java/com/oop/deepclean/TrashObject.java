9cec46ed0cfff94d87467b8fec151b8bbd053001
Ysmael.Mediavillo <ysmaelbaccay.mediavillo25@bicol-u.edu.ph>
2025-12-30 13:50:38 +0800

    Update TrashObject.java

--- src/main/java/com/oop/deepclean/TrashObject.java
+++ src/main/java/com/oop/deepclean/TrashObject.java
@@ -1,12 +1,11 @@
 package com.oop.deepclean;
-
 import java.awt.Image;
 import java.awt.Rectangle;
 
 public class TrashObject {
-    public int x, y;
-    public Rectangle bounds;
-    public Image sprite;
+    private int x, y;
+    private Rectangle bounds;
+    private Image sprite;
 
     public TrashObject(int x, int y, Image sprite) {
         this.x = x;
@@ -14,4 +13,9 @@ public class TrashObject {
         this.sprite = sprite;
         this.bounds = new Rectangle(x, y, 40, 40);
     }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public Rectangle getBounds() { return bounds; }
+    public Image getSprite() { return sprite; }
 }
